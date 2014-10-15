348c620ba4e9310ca925a3c4795b440f9166891b
Rusty Conover <rusty+cpan@luckydinosaur.com>
2014-10-15 14:36:39 +0000

    Import of RCONOVER/VM-EC2-Security-CredentialCache-0.002 from CPAN.
    gitpan-cpan-distribution: VM-EC2-Security-CredentialCache
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         RCONOVER/VM-EC2-Security-CredentialCache-0.002.tar.gz
    gitpan-cpan-author:       RCONOVER
    gitpan-cpan-maturity:     released

--- lib/VM/EC2/Security/CredentialCache.pm
+++ lib/VM/EC2/Security/CredentialCache.pm
@@ -1,5 +1,5 @@
 package VM::EC2::Security::CredentialCache;
-$VM::EC2::Security::CredentialCache::VERSION = '0.001';
+
 use strict;
 use warnings;
 use DateTime::Format::ISO8601;
@@ -20,7 +20,7 @@ Retrieves the current EC2 instance's IAM credentials and caches them until they 
 
 =head1 DESCRIPTION
 
-This module provides a cache for an EC2's IAM credentials represented by L<VM::EC2::SecurityCredentials>. 
+This module provides a cache for an EC2's IAM credentials represented by L<VM::EC2::Security::Credentials>. 
 Rather than retriving the credentials for every possible call that uses them, cache them until they
 expire and retreive them again if they have expired.
 
