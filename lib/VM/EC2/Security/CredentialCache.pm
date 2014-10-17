ce9470f9adcd3aecbeaaab0702a297148d7c0e78
Rusty Conover <rusty+cpan@luckydinosaur.com>
2014-10-17 04:48:45 +0000

    Import of RCONOVER/VM-EC2-Security-CredentialCache-0.14 from CPAN.
    gitpan-cpan-distribution: VM-EC2-Security-CredentialCache
    gitpan-cpan-version:      0.14
    gitpan-cpan-path:         RCONOVER/VM-EC2-Security-CredentialCache-0.14.tar.gz
    gitpan-cpan-author:       RCONOVER
    gitpan-cpan-maturity:     released

--- lib/VM/EC2/Security/CredentialCache.pm
+++ lib/VM/EC2/Security/CredentialCache.pm
@@ -1,5 +1,5 @@
 package VM::EC2::Security::CredentialCache;
-$VM::EC2::Security::CredentialCache::VERSION = '0.12';
+$VM::EC2::Security::CredentialCache::VERSION = '0.14';
 use strict;
 use warnings;
 use DateTime::Format::ISO8601;
@@ -30,6 +30,7 @@ my $credentials;
 my $credential_expiration_dt;
 
 sub get {
+  RETRY:
     if (!defined($credentials)) {
         my $meta = VM::EC2::Instance::Metadata->new;
         defined($meta) || die("Unable to retrieve instance metadata");
@@ -40,11 +41,11 @@ sub get {
 
     if ($credential_expiration_dt->subtract_datetime_absolute(DateTime->now())->is_positive()) {
         return $credentials;
-    } 
-    
-    $credentials = undef;
-    $credential_expiration_dt = undef;
-    return get_credentials();
+    } else {
+        $credentials = undef;
+        $credential_expiration_dt = undef;
+        goto RETRY;
+    }
 }
 
 1;
